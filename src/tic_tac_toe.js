5f6702182ff58a86beaff0d5a51393a436d631d8
Jyothi Praveen Gorre <jyothipraveengorre@Jyothis-MacBook-Pro.local>
2025-11-22 12:42:17 +0530

    searching in the diagonal positions is done ,now working on the horizontal locations

--- src/tic_tac_toe.js
+++ src/tic_tac_toe.js
@@ -7,13 +7,23 @@ const positions = {
   "6": [1, 2],
   "7": [2, 0],
   "8": [2, 1],
-  "9": [2, 2]
+  "9": [2, 2],
 };
 
 export const ticTacToe = () => {
   return "";
 };
 
-export const areOnHorizontal;
+export const areOnHorizontalLine = (locations) => {
+  const areOnHorizontal = [[1, 2, 3], [4, 5, 6]];
+  return areOnHorizontal.some((horizontalLine) =>
+    locations.every((position) => horizontalLine.includes(position))
+  );
+};
 
-ticTacToe();
+export const areOnDiagonal = (locations) => {
+  const diagonalPositions = [[1, 5, 9], [3, 5, 7]];
+  return diagonalPositions.some((diagonal) =>
+    locations.every((position) => diagonal.includes(position))
+  );
+};
--- test.js/tic_tac_toe_test.js
+++ test.js/tic_tac_toe_test.js
@@ -1,11 +1,46 @@
 import { assertEquals } from "@std/assert";
-import { ticTacToe } from "../src/tic_tac_toe.js";
+import {
+  areOnDiagonal,
+  areOnHorizontalLine,
+  ticTacToe,
+} from "../src/tic_tac_toe.js";
 
 Deno.test("simple test case", () => {
-  assertEquals(ticTacToe(), '')
+  assertEquals(ticTacToe(), "");
 });
 
-Deno.test("are positions on horizontal", () => {
-  assertEquals(ticTacToe(), [1, 2, 3])
+Deno.test("this is not in Diagonal positions", () => {
+  assertEquals(areOnDiagonal([1, 2, 3]), false);
 });
 
+Deno.test("this is in Diagonal positions", () => {
+  assertEquals(areOnDiagonal([1, 5, 9]), true);
+});
+
+Deno.test("this is in Diagonal positions", () => {
+  assertEquals(areOnDiagonal([3, 5, 7]), true);
+});
+
+Deno.test("this is not in Diagonal positions", () => {
+  assertEquals(areOnDiagonal([3, 5, 8]), false);
+});
+
+Deno.test("this is not in Diagonal positions", () => {
+  assertEquals(areOnDiagonal([1, 5, 3]), false);
+});
+
+Deno.test("this is also not in Diagonal positions", () => {
+  assertEquals(areOnDiagonal([7, 5, 9]), false);
+});
+
+Deno.test("1st horizontal line", () => {
+  assertEquals(areOnHorizontalLine([1, 2, 3]), true);
+});
+
+Deno.test("2nd horizontal line", () => {
+  assertEquals(areOnHorizontalLine([4, 5, 6]), true);
+});
+
+Deno.test("3rd horizontal line", () => {
+  assertEquals(areOnHorizontalLine([7, 8, 9]), true);
+});
